84734c261c20a6a2bd5a474f4516b1b6b56508b2
Mark Woodhall <mark.woodhall@bybox.com>
2014-05-23 21:09:36 +0100

    Update project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ravenclo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A clojure library for consuming a RavenDB rest api."
+  :url "https://bitbucket.org/markwoodhall/raven-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
