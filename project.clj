d51f017c3da869680e058d93117818e1f0d96da7
Mark Woodhall <mark.woodhall@bybox.com>
2014-06-06 22:02:42 +0100

    Minor update to project.clj, remove pprint from client.

--- project.clj
+++ project.clj
@@ -1,8 +1,14 @@
-(defproject ravenclo "0.1.0-SNAPSHOT"
+(defproject markwoodhall/raven-clj "0.1.0"
   :description "A clojure library for consuming a RavenDB rest api."
   :url "https://bitbucket.org/markwoodhall/raven-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[quickie "0.2.5"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.7"]
-                 [org.clojure/data.json "0.2.3"]])
+                 [org.clojure/data.json "0.2.3"]]
+  :deploy-repositories [
+    ["clojars" {:sign-releases false}]
+  ]
+  :scm {:name "bitbucket"
+        :url "https://bitbucket.org/markwoodhall/raven-clj"})
