ec007ef09dd54e20fdae21438ad740bf5ac00ce1
Mark Woodhall <mark.woodhall@bybox.com>
2014-06-08 07:23:12 +0100

    Upgrade clj-http to latest version.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-ravendb "0.1.1"
+(defproject clj-ravendb "0.1.2"
   :description "A clojure library for consuming a RavenDB rest api."
   :url "https://bitbucket.org/markwoodhall/clj-ravendb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[quickie "0.2.5"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.7"]
+                 [clj-http "0.9.2"]
                  [org.clojure/data.json "0.2.3"]]
   :deploy-repositories [
     ["clojars" {:sign-releases false}]
