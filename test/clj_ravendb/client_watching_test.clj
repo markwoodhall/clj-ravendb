9e417252b2cc9dba45f3a563605d3947bef36033
Mark Woodhall <mark.woodhall@bybox.com>
2014-06-17 21:44:46 +0100

    future-cancel does not always have the desired effect. Instead close over a local atom 'keep-running?' and reset it to false on stop.

--- test/clj_ravendb/client_watching_test.clj
+++ test/clj_ravendb/client_watching_test.clj
@@ -15,22 +15,24 @@
       (let [key "TestDocToWatch"
             document {:test 2 :name "WatchedDocument"}
             ch (chan)
-            watcher (watch-documents client ["TestDocToWatch"] ch)]
-        (Thread/sleep 1000)
-        (put-document client key document) 
-        (is (= (first (:results (<!! ch))) {:key key :doc document}))
-        ((:stop watcher)))))
+            watcher (watch-documents client ["TestDocToWatch"] ch)
+            _ (Thread/sleep 1000)
+            _ (put-document client key document) 
+            actual (first (:results (<!! ch)))] 
+        ((:stop watcher))   
+        (is (= actual {:key key :doc document})))))
 
   (deftest test-watching-index-puts-to-channel-on-index-change
     (testing "Watching a index puts to a channel on index change"
       (let [key "TestDocToWatch"
             document {:test 2 :name "WatchedDocument"}
             ch (chan)
-            watcher (watch-index client {:index "WatchedDocuments"} ch)]
-        (Thread/sleep 1000)
-        (put-document client key document) 
-        (is (= (first (:results (<!! ch))) document))
-        ((:stop watcher)))))
+            watcher (watch-index client {:index "WatchedDocuments"} ch)
+            _ (Thread/sleep 1000)
+            _ (put-document client key document) 
+            actual (first (:results (<!! ch)))]
+        ((:stop watcher))   
+        (is (= actual document)))))
 
   (use-fixtures :each (fn [f] 
                         (put-index client {:name "WatchedDocuments" 
