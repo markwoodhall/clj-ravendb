6fc1567b33923c70baa07dd02a3f0af7af3c03a4
Mark Woodhall <mark.woodhall@gmail.com>
2015-03-12 21:29:49 +0000

    Remove trailing whitespace and bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-ravendb "0.4.2-SNAPSHOT"
+(defproject clj-ravendb "0.5.0"
   :description "A Clojure library for consuming a RavenDB rest api."
   :url "https://github.com/markwoodhall/clj-ravendb"
   :license {:name "MIT License"
@@ -6,7 +6,7 @@
   :plugins [[quickie "0.2.5"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.9.2"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]                
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.clojure/data.json "0.2.3"]]
   :deploy-repositories [
     ["clojars" {:sign-releases false}]
