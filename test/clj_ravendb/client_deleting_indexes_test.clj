b5fd8c600e7503c4a7c5f1a09ba60c95e8d8c695
Mark Woodhall <mark.woodhall@gmail.com>
2015-03-22 23:13:38 +0000

    Create indexes with more unique names because of concurrent testing in travis.

--- test/clj_ravendb/client_deleting_indexes_test.clj
+++ test/clj_ravendb/client_deleting_indexes_test.clj
@@ -6,19 +6,20 @@
             [clj-ravendb.config :refer :all]))
 
 (let [caching-client (client ravendb-url ravendb-database {:caching :aggressive :ssl-insecure? true :oauth-url oauth-url :api-key api-key})
-      client (client ravendb-url ravendb-database {:ssl-insecure? true :oauth-url oauth-url :api-key api-key})]
-  (doseq [i ["test-index" "test-index2"]]
+      client (client ravendb-url ravendb-database {:ssl-insecure? true :oauth-url oauth-url :api-key api-key})
+      indexes [(str "test-index" (System/currentTimeMillis)) (str "test-index" (System/currentTimeMillis))]]
+  (doseq [i indexes]
     (put-index! client {:index i
                         :where [[:== :name "TestDocument"]]
                         :select [:name]}))
   (deftest test-delete-index-returns-correct-status-code
     (testing "deleting an index returns the correct status code"
-      (let [actual (delete-index! client "test-index")
+      (let [actual (delete-index! client (first indexes))
             expected 204]
         (is (= expected (actual :status))))))
 
   (deftest test-delete-index-returns-correct-status-code-when-using-caching-client
     (testing "deleting an index returns the correct status code"
-      (let [actual (delete-index! caching-client "test-index2")
+      (let [actual (delete-index! caching-client (second indexes))
             expected 204]
         (is (= expected (actual :status)))))))
