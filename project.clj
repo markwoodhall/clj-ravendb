6c0d8fd881b5e443f3337cd3ea690e4f28ee96cf
Mark Woodhall <mark.woodhall@gmail.com>
2016-04-28 19:55:23 +0100

    Remove quickie plugin.

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "https://github.com/markwoodhall/clj-ravendb"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
-  :plugins [[quickie "0.2.5"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.1.0"]
                  [cheshire "5.4.0"]
