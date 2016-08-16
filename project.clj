89b6bc24cf472edb865cb4fe81f6e033b3226686
Mark Woodhall <mark.woodhall@gmail.com>
2016-08-16 20:59:53 +0100

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-ravendb "1.0.1"
+(defproject clj-ravendb "1.2.0"
   :description "A Clojure library designed to consume a RavenDB HTTP API."
   :url "https://github.com/markwoodhall/clj-ravendb"
   :license {:name "MIT License"
